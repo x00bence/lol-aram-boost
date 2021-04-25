2ab0d9132efcf6680c30c93594ccc6dff6e1e73c
Kovács Bence <60524991+x00bence@users.noreply.github.com>
2021-04-26 01:51:06 +0200

    ⚡ We're still alive and kicking!
    Updated to use the latest method. Minor code changes, now runs in a loop. Added .bat file for lazy people.

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# Update
-
-### Riot Games has cracked down on numerous community-made scripts as of recently (February 23, 2021), and extra security measures were introduced. The endpoint that lol-aram-boost depended on has been completely deprecated and made unusable for its original purpose. The repository itself will stay up, although in an archived state, as I believe regardless of its usability, it can still be salvaged and be used to learn the basics of the League of Legends Client API. 
-
 # lol-aram-boost
 
-Skin boosts a League of Legends ARAM lobby without consuming any RP, temporarily unlocking you a skin and granting extra BE at the end of your match
+Skin boosts a League of Legends ARAM lobby without consuming any RP, temporarily unlocking you a skin and granting extra BE at the end of your match.
 
 # Usage
 
-1. Get into an ARAM champion select lobby
-2. Run the script
+1. Open your client and/or get into an ARAM champion select lobby
+2. Run the script (Note: Run as administrator if your client is also running as that)
 3. Your lobby should now be skin boosted!
+
+# History
+
+- September 13, 2020: lol-aram-boost is released.
+- February 23, 2021: Riot Games cracks down on user made scripts, in turn patching lol-aram-boost. A new method is found not much later.
+- April 26, 2021: lol-aram-boost is updated to use the new method.
--- aram.py
+++ aram.py
@@ -1,28 +1,38 @@
 import subprocess
 import re
-import os
 import urllib3
 import requests
 
-# Disables the annoying error
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+if __name__ == '__main__':
+    try:        
+        print('$ lol-aram-boost | x00bence\n')
 
-command = "WMIC PROCESS WHERE name='LeagueClientUx.exe' GET commandline"
+        # Batch command to get the client process
+        command = "WMIC PROCESS WHERE name='LeagueClientUx.exe' GET commandline"
 
-output = subprocess.Popen(command, stdout=subprocess.PIPE,
-                          shell=True).stdout.read().decode('utf-8')
+        # Execute the command
+        output = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                shell=True).stdout.read().decode('utf-8')
 
-port = re.findall(r'"--app-port=(.*?)"', output)[0]
-password = re.findall(r'"--remoting-auth-token=(.*?)"', output)[0]
+        # Extract needed args
+        port = re.findall(r'"--app-port=(.*?)"', output)[0]
+        password = re.findall(r'"--remoting-auth-token=(.*?)"', output)[0]
 
-print('Connected to League ...')
+        # Disables the annoying certificate error
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-session = requests.session()
-session.verify = False
+        # Set up session
+        session = requests.session()
+        session.verify = False
 
-session.post('https://127.0.0.1:%s/lol-champ-select/v1/team-boost/purchase' %
-             port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
+        print('[*] Connected to League; Enter to boost lobby, ^C to exit.')
 
-print('Boosted the lobby!')
+        # Loop
+        while True:
+            input()
+            session.post('https://127.0.0.1:%s/lol-login/v1/session/invoke?destination=lcdsServiceProxy&method=call&args=["","teambuilder-draft","activateBattleBoostV1",""]' %
+                    port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
+            print('[+] Boosted the lobby!')
+    except KeyboardInterrupt:
+        exit()
 
-input("Press any key to continue . . .")
